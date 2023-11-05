4c2acd0fd1c92449ae8d2ddc577769b2c9367bd0
YusufHasan94 <yhpolok@gmail.com>
2023-11-05 20:39:01 +0600

    implement sorting features

--- src/Components/Gallery/Gallery.jsx
+++ src/Components/Gallery/Gallery.jsx
@@ -1,24 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from "react";
 import "./Gallery.css";
 import { FaImage } from "react-icons/fa6";
+import Card from "../Card/Card";
 
-const Gallery = () => {
-    const [images, setImages] = useState([]);
-    useEffect(()=>{
-        fetch('/gallery.json')
-        .then(res=>res.json())
-        .then(data=>setImages(data));
-    },[]);
+//creating image gallery 
+
+const Gallery = ({ setSelectedImages, images, moveImage }) => {
+    const [selectedItems, setSelectedItems] = useState([]);
+
+    const handleSelected = (id) => {
+        if (selectedItems.includes(id)) {
+            setSelectedItems(selectedItems.filter(imgId => {
+                imgId !== id
+            }));
+        }
+        else {
+            setSelectedItems([...selectedItems, id]);
+        }
+    }
+    setSelectedImages(selectedItems);
+
     return (
         <div >
             <div className='grid grid-cols-5 col gap-5 py-10'>
                 {
                     images.map((image, index) => (
-                        <div key={image.id} className={"rounded-xl overflow-hidden relative hover:z-10 hover:opacity-50 border-gray-300 border-2"+
-                                        (index === 0? " featureImage": "")}>
-                            <input type="checkbox" className='absolute top-4 left-4 opacity-0 hover:opacity-100 z-50' name="" id="" />
-                            <img src={image.url} alt="" className=''/>
-                        </div>
+                            <div key={image.id} className={"rounded-xl overflow-hidden relative hover:z-10 hover:opacity-50 border-gray-300 border-2" +
+                                (index === 0 ? " featureImage" : "")}>
+                                <input
+                                    type="checkbox"
+                                    className='absolute top-4 left-4 z-50'
+                                    name=""
+                                    id=""
+                                    onChange={() => handleSelected(image.id)}
+                                />
+                                <Card
+                                    id={image.id}
+                                    src={image.url}
+                                    index={index}
+                                    moveImage={moveImage}
+                                />
+                            </div>
                     ))
                 }
                 {
@@ -26,7 +48,7 @@ const Gallery = () => {
                         <div className=''>
                             <input type="file" name="" id="" className='opacity-0' />
                             <div className='flex flex-col justify-center items-center'>
-                                <FaImage className='text-2xl'/>
+                                <FaImage className='text-2xl' />
                                 <h1 className='text-xl mt-2'>Add Images</h1>
                             </div>
                         </div>
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,26 +1,54 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Gallery from '../gallery/gallery';
 
 const Main = () => {
+    const [images, setImages] = useState([]);
+    const [selectedImages, setSelectedImages] = useState([]);
+    useEffect(()=>{
+        fetch('/gallery.json') //fetch image from json
+        .then(res=>res.json())
+        .then(data=>setImages(data));
+    },[]);
+    
+    const handleDelete = ()=>{
+        const remainingImages = images.filter((image)=>{
+            return !selectedImages.includes(image.id);
+        })
+        setSelectedImages([]);
+        setImages(remainingImages);
+    }
+
+    const moveImage = useCallback((dragIndex, hoverIndex)=>{
+        setImages((prevCards)=>{
+            const cloneCards = [...prevCards];
+            const removedItems = cloneCards.splice(dragIndex, 1)[0];
+            cloneCards.splice(hoverIndex, 0, removedItems);
+            return cloneCards;
+        });
+    },[])
+
     return (
         <div className='py-20 max-w-screen-xl mx-auto '>
-            <h1 className='text-4xl text-center font-semibold mb-10'>
-                Gallery 
-            </h1>
             <div className='bg-slate-50 mt-10 p-10 rounded-xl'>
                 <div className='md:flex justify-between'>
                     <div className='md:flex gap-5'>
-                        <input type="checkbox" name="" id="" />
-                        <h1 className='text-xl'>0 File selected</h1>
+                        {
+                            selectedImages.length ===0?
+                            <h1 className='text-4xl font-semibold'>Gallery</h1>:
+                            <div className='flex items-center gap-5 text-2xl'>
+                                <input type="checkbox" name="" checked id=""/>
+                                <h1>{`${selectedImages.length} File selected`}</h1>
+                            </div>
+                        }                            
                     </div>
                     <div>
-                        <button className='text-red-600 font-semibold text-xl hover:text-red-400'>
+                        <button onClick={handleDelete} className='text-red-600 font-semibold text-xl hover:text-red-400'>
                             Delete Files
                         </button>
                     </div>
                 </div>
                 <div>
-                    <Gallery/>
+                    <Gallery setSelectedImages={setSelectedImages} images={images} moveImage={moveImage}/>
                 </div>
             </div>
         </div>
